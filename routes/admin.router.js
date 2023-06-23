cdb5ec6f6624e7db5f9ec9c975cd335ebf66a1f6
Anandhu_P_A <anandhupa131@gmail.com>
2023-06-23 15:24:50 +0530

    user management table linked to profile page of user as an admin functionality

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -10,11 +10,34 @@ const router = require("express").Router();
 router.get("/allusers",ensureAuthenticated,ensureAdmin,async(req,res,next)=>{
     try {
         let users = await UserModel.find();
-        res.send(users)
+        res.render("userlist",{users})
     } catch (error) {
         console.log(error);next(error)
     }
 })
+
+router.get("/user/:id",async(req,res,next)=>{
+    try {
+         let user = await UserModel.findById(req.params.id);
+        res.render("profile",{user,adminView:true})
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+
+// update role -- only for admin 
+
+router.post("/update-role",async(req,res,next)=>{
+    try {
+        res.redirect("/")
+    } catch (error) {
+        next(error)
+    }
+})
+
+
 
 
 module.exports=router;
