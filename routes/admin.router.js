45d3b613ce6e2abfbfdab1459ecd7628c64f7bab
Anandhu_P_A <anandhupa131@gmail.com>
2023-06-23 16:53:37 +0530

    role based access control implimented on admin page(in user role change access)

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -29,15 +29,42 @@ router.get("/user/:id",async(req,res,next)=>{
 
 // update role -- only for admin 
 
-router.post("/update-role",async(req,res,next)=>{
+router.post("/update-role",validate,async(req,res,next)=>{
     try {
-        res.redirect("/")
+     
+      
+        let out = await UserModel.findByIdAndUpdate(req.body.id, {role:req.body.role},{new:true});
+        res.render("profile",{user:out,adminView:true});
+       
     } catch (error) {
         next(error)
     }
 })
 
 
+async function validate (req,res,next){
+    let users = await UserModel.find();
+    
+    if(!req.body.id || !req.body.role){
+        req.flash("danger","please fill all the entries");
+       
+        const messages = req.flash();
+        res.render("userlist",{users,messages})}
+          
+          else if(req.session.user.role!=="ADMIN"){
+            req.flash("danger","As a user , you don't have access to change the role of users")
+            const messages = req.flash();
+            res.render("userlist",{users,messages})}
+          else if(req.session.user._id===req.body.id){
+            req.flash("danger","As an admin ,you are not allowed to update your own role , ask any other admin to change it!!")
+            const messages = req.flash();
+            res.render("userlist",{users,messages})
+        }else {
+            next()
+        }
 
+     
+       
+}
 
 module.exports=router;
