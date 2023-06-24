19a4ced1b66fed9a1fd2e3908e9ec47763e0c533
Anandhu_P_A <anandhupa131@gmail.com>
2023-06-24 11:07:48 +0530

    about section and home page design added

--- routes/index.route.js
+++ routes/index.route.js
@@ -6,6 +6,11 @@ router.get("/",async(req,res,next)=>{
         res.render("index")
     }catch(err){console.log("error in index router",err)}
 })
+router.get("/documentation",async(req,res,next)=>{
+    try{
+        res.render("documentation")
+    }catch(err){console.log("error in index router",err)}
+})
 
 
 module.exports=router;
